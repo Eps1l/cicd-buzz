91a59340141d74c99f74011938e6857e3498f464
root <eps1lswork@gmail.com>
2020-02-20 06:43:14 -0500

    Add log

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+echo "Step 1 Login" >> log
+docker login -u $DOCKER_USER -p $DOCKER_PASS >> log
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $DOCKER_USER/$TRAVIS_REPO_SLUG:$TAG .
-docker push $DOCKER_USER/$TRAVIS_REPO_SLUG
+echo "Step 2 Build" >> log
+docker build -f Dockerfile -t $DOCKER_USER/$TRAVIS_REPO_SLUG:$TAG .  >> log
+echo "Step 1 Push" >> log
+docker push $DOCKER_USER/$TRAVIS_REPO_SLUG  >> log
